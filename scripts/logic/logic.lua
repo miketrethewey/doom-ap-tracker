0cd38e616a79d855b2cfe7358056958e205de956
Minnie A. Trethewey (Mike) <minnietrethewey@gmail.com>
2024-02-20 22:14:44 -0800

    Simplify logic

--- scripts/logic/logic.lua
+++ scripts/logic/logic.lua
@@ -8,344 +8,106 @@ function has(item, amount)
     end
 end
 
-function e1m2_redaccess()
+function key_access(level, color)
     return (
-        has("e1m2_access") and has("e1m2_red")
+        has(level .. "_access") and has(level .. "_" .. color)
     )
 end
 
-function e1m3_blueaccess()
-    return (
-        has("e1m3_access") and has("e1m3_blue")
-    )
-end
-
+-- blue && yellow
 function e1m3_blueyellowaccess()
-    return (
-        has("e1m3_access") and has("e1m3_blue") and has("e1m3_yellow")
-    )
+    return key_access("e1m3", "blue") and key_access("e1m3", "yellow")
 end
 
+-- blue || yellow
 function e1m4_blueyellow()
-    return (
-        has("e1m4_blue") or has("e1m4_yellow")
-    )
+    return key_access("e1m4", "blue") or key_access("e1m4", "yellow")
 end
 
-function e1m5_yellowaccess()
-    return (
-        has("e1m5_access") and has("e1m5_yellow")
-    )
-end
-
+-- blue && yellow
 function e1m5_blueyellowaccess()
-    return (
-        has("e1m5_access") and (has("e1m5_blue") and has("e1m5_yellow"))
-    )
+    return key_access("e1m5", "blue") and key_access("e1m5", "yellow")
 end
 
-function e1m6_blueaccess()
-    return (
-        has("e1m6_access") and has("e1m6_blue")
-    )
-end
-
+-- blue || yellow
 function e1m6_blueyellowaccess()
-    return (
-        has("e1m6_access") and (has("e1m6_blue") or has("e1m6_yellow"))
-    )
+    return key_access("e1m6", "blue") or key_access("e1m6", "yellow")
 end
 
-function e1m6_redaccess()
-    return (
-        has("e1m6_access") and has("e1m6_red")
-    )
-end
-
+-- blue && yellow
 function e1m7_blueyellowaccess()
-    return (
-        has("e1m7_access") and (has("e1m7_blue") and has("e1m7_yellow"))
-    )
+    return key_access("e1m7", "blue") and key_access("e1m7", "yellow")
 end
 
-function e1m7_yellowaccess()
-    return (
-        has("e1m7_access") and has("e1m7_yellow")
-    )
-end
-
-function e1m7_redaccess()
-    return (
-        has("e1m7_access") and has("e1m7_red")
-    )
-end
-
+-- yellow && red
 function e1m7_yellowredaccess()
-    return (
-        has("e1m7_access") and (has("e1m7_yellow") and has("e1m7_red"))
-    )
+    return key_access("e1m7", "yellow") and key_access("e1m7", "red")
 end
 
-function e1m9_blueaccess()
-    return (
-        has("e1m9_access") and has("e1m9_blue")
-    )
-end
-
-function e1m9_yellowaccess()
-    return (
-        has("e1m9_access") and has("e1m9_yellow")
-    )
-end
-
-function e1m9_redaccess()
-    return (
-        has("e1m9_access") and has("e1m9_red")
-    )
-end
-
-function e2m2_blueaccess()
-    return (
-        has("e2m2_access") and has("e2m2_blue")
-    )
-end
-
-function e2m2_yellowaccess()
-    return (
-        has("e2m2_access") and has("e2m2_yellow")
-    )
-end
-
-function e2m3_blueaccess()
-    return (
-        has("e2m3_access") and has("e2m3_blue")
-    )
-end
-
-function e2m4_blueaccess()
-    return (
-        has("e2m4_access") and has("e2m4_blue")
-    )
-end
-
+-- blue && yellow
 function e2m4_blueyellowaccess()
-    return (
-        has("e2m4_access") and (has("e2m4_blue") and has("e2m4_yellow"))
-    )
+    return key_access("e2m4", "blue") and key_access("e2m4", "yellow")
 end
 
-function e2m6_yellowaccess()
-    return (
-        has("e2m6_access") and has("e2m6_yellow")
-    )
-end
-
+-- blue && yellow && red
 function e2m6_blueyellowredaccess()
-    return (
-        has("e2m6_access") and (has("e2m6_blue") and has("e2m6_yellow") and has("e2m6_red"))
-    )
+    return key_access("e2m6", "blue") and key_access("e2m6", "yellow") and key_access("e2m6", "red")
 end
 
-function e2m7_blueaccess()
-    return (
-        has("e2m7_access") and has("e2m7_blue")
-    )
-end
-
-function e2m7_yellowaccess()
-    return (
-        has("e2m7_access") and has("e2m7_yellow")
-    )
-end
-
+-- blue && red
 function e2m7_blueredaccess()
-    return (
-        has("e2m7_access") and (has("e2m7_blue") and has("e2m7_red"))
-    )
+    return key_access("e2m7", "blue") and key_access("e2m7", "red")
 end
 
-function e2m9_blueaccess()
-    return (
-        has("e2m9_access") and has("e2m9_blue")
-    )
-end
-
-function e2m9_redaccess()
-    return (
-        has("e2m9_access") and has("e2m9_red")
-    )
-end
-
-function e2m9_yellowaccess()
-    return (
-        has("e2m9_access") and has("e2m9_yellow")
-    )
-end
-
-function e3m2_blueaccess()
-    return (
-        has("e3m2_access") and has("e3m2_blue")
-    )
-end
-
-function e3m3_blueaccess()
-    return (
-        has("e3m3_access") and has("e3m3_blue")
-    )
-end
-
-function e3m4_blueaccess()
-    return (
-        has("e3m4_access") and has("e3m4_blue")
-    )
-end
-
+-- blue && yellow
 function e3m4_blueyellowaccess()
-    return (
-        has("e3m4_access") and (has("e3m4_blue") and has("e3m4_yellow"))
-    )
+    return key_access("e3m4", "blue") and key_access("e3m4", "yellow")
 end
 
+-- blue && red
 function e3m4_blueredaccess()
-    return (
-        has("e3m4_access") and (has("e3m4_blue") and has("e3m4_red"))
-    )
+    return key_access("e3m4", "blue") and key_access("e3m4", "red")
 end
 
-function e3m5_blueaccess()
-    return (
-        has("e3m5_access") and has("e3m5_blue")
-    )
-end
-
-function e3m5_yellowaccess()
-    return (
-        has("e3m5_access") and has("e3m5_yellow")
-    )
-end
-
-function e3m6_blueaccess()
-    return (
-        has("e3m6_access") and has("e3m6_blue")
-    )
-end
-
-function e3m7_blueaccess()
-    return (
-        has("e3m7_access") and has("e3m7_blue")
-    )
-end
-
-function e3m7_redaccess()
-    return (
-        has("e3m7_access") and has("e3m7_red")
-    )
-end
-
+-- yellow && red
 function e3m7_yellowredaccess()
-    return (
-        has("e3m7_access") and (has("e3m7_yellow") and has("e3m7_red"))
-    )
+    return key_access("e3m7", "yellow") and key_access("e3m7", "red")
 end
 
-function e3m9_blueaccess()
-    return (
-        has("e3m9_access") and has("e3m9_blue")
-    )
-end
-
+-- defined but not used?
+-- red
 function e3m9_redaccess()
     return (
         has("e3m9_access") and has("e3m9_red")
     )
 end
 
+-- blue && red
 function e3m9_blueredaccess()
-    return (
-        has("e3m9_access") and (has("e3m9_blue") and has("e3m9_red"))
-    )
+    return key_access("e3m9", "blue") and key_access("e3m9", "red")
 end
 
-function e4m2_blueaccess()
-    return (
-        has("e4m2_access") and has("e4m2_blue")
-    )
-end
-
-function e4m2_yellowaccess()
-    return (
-        has("e4m2_access") and has("e4m2_yellow")
-    )
-end
-
+-- blue || yellow
 function e4m2_blueyellowaccess()
-    return (
-        has("e4m2_access") and (has("e4m2_blue") or has("e4m2_yellow"))
-    )
+    return key_access("e4m2", "blue") or key_access("e4m2", "yellow")
 end
 
+-- blue && red
 function e4m3_blueredaccess()
-    return (
-        has("e4m3_access") and (has("e4m3_blue") and has("e4m3_red"))
-    )
+    return key_access("e4m3", "blue") and key_access("e4m3", "red")
 end
 
-function e4m4_redaccess()
-    return (
-        has("e4m4_access") and has("e4m4_red")
-    )
-end
-
-function e4m5_redaccess()
-    return (
-        has("e4m5_access") and has("e4m5_red")
-    )
-end
-
-function e4m6_blueaccess()
-    return (
-        has("e4m6_access") and has("e4m6_blue")
-    )
-end
-
+-- blue && red
 function e4m6_blueredaccess()
-    return (
-        has("e4m6_access") and (has("e4m6_blue") and has("e4m6_red"))
-    )
+    return key_access("e4m6", "blue") and key_access("e4m6", "red")
 end
 
+-- blue && red
 function e4m6_bluered()
-    return (
-        has("e4m6_blue") and has("e4m6_red")
-    )
+    return key_access("e4m6", "blue") and key_access("e4m6", "red")
 end
 
-function e4m7_blueaccess()
-    return (
-        has("e4m7_access") and has("e4m7_blue")
-    )
-end
-
-function e4m7_yellowaccess()
-    return (
-        has("e4m7_access") and has("e4m7_yellow")
-    )
-end
-
-function e4m7_redaccess()
-    return (
-        has("e4m7_access") and has("e4m7_red")
-    )
-end
-
+-- yellow && red
 function e4m8_yellowredaccess()
-    return (
-        has("e4m8_access") and (has("e4m8_yellow") and has("e4m8_red"))
-    )
-end
-
-function e4m9_yellowaccess()
-    return (
-        has("e4m9_access") and has("e4m9_yellow")
-    )
+    return key_access("e4m8", "yellow") and key_access("e4m8", "red")
 end
