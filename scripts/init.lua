dae7e6900f8e07f4ce090f8ab876bdbbcb44b3df
Minnie A. Trethewey (Mike) <minnietrethewey@gmail.com>
2024-02-20 21:41:04 -0800

    Use overlays, fix lua order

--- scripts/init.lua
+++ scripts/init.lua
@@ -1,18 +1,20 @@
 local variant = Tracker.ActiveVariantUID
 IS_UNLABELLED = variant:find("maps-u")
 
+-- Items
+print("Loading Items")
 Tracker:AddItems("items/items.json")
 
 -- Logic
+print("Loading Logic")
 ScriptHost:LoadScript("scripts/logic/logic.lua")
 
 -- Maps
+print("Loading Maps")
 Tracker:AddMaps("maps/maps.json")
 
--- Locations
-Tracker:AddLocations("locations/locations.json")
-
 -- Layout
+print("Loading Layouts")
 Tracker:AddLayouts("layouts/levels.json")
 Tracker:AddLayouts("layouts/keys.json")
 Tracker:AddLayouts("layouts/weapons.json")
@@ -20,6 +22,10 @@ Tracker:AddLayouts("layouts/settings.json")
 Tracker:AddLayouts("layouts/tabs.json")
 Tracker:AddLayouts("layouts/tracker.json")
 Tracker:AddLayouts("layouts/broadcast.json")
+
+-- Locations
+print("Loading Locations")
+Tracker:AddLocations("locations/locations.json")
 
 -- AutoTracking for Poptracker
 if PopVersion and PopVersion >= "0.18.0" then
