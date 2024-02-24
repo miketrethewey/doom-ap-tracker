f425a5090734a02514a9203052da83110e419251
Minnie A. Trethewey (Mike) <minnietrethewey@gmail.com>
2024-02-23 20:58:37 -0800

    Split levels

---
+++ resources/ci/common/eptolevel.py
@@ -0,0 +1,54 @@
+import json
+import os
+
+locationsPath = os.path.join(
+    "locations",
+    "locations.json"
+)
+if os.path.isfile(locationsPath):
+    with open(locationsPath, "r") as locationsFile:
+        locations = json.load(locationsFile)
+        for mapData in locations:
+            mapName = mapData["name"]
+            if "E" in mapName:
+                epID = mapName.lower()[1:2]
+                mapID = mapName.lower()[3:4]
+                del mapData["chest_unopened_img"]
+                del mapData["chest_opened_img"]
+                for child in mapData["children"]:
+                    if child["name"] == "Exit":
+                        child["map_locations"][0]["size"] = 20
+                    child["map_locations"][0]["visibility_rules"] = child["visibility_rules"]
+                    del child["visibility_rules"]
+                    ref = ""
+                    for section in child["sections"]:
+                        section["ref"] = section["ref"].replace("Mars/","")
+                        ref = section["ref"]
+                    mapData["parent"] = ref[:ref.find('/')]
+                    child["access_rules"] =  [ f"@{ref}" ]
+                entrance = {
+                    "name": "Entrance",
+                    "chest_unopened_img": "/images/items/levelstart.png",
+                    "chest_opened_img": "/images/items/levelstart.png",
+                    "access_rules": [ "$access|null" ],
+                    "sections": [
+                        {
+                            "name": "Level Start",
+                            "item_count": 1
+                        }
+                    ],
+                    "map_locations": [
+                        {
+                            "map": f"e{epID}m{mapID}",
+                            "visibility_rules": [ f"ep{epID}_on" ],
+                            "size": 20,
+                            "x": 0,
+                            "y": 0
+                        }
+                    ]
+                }
+                mapData["children"].insert(0,entrance)
+                mapData = [mapData]
+                print(mapData)
+                with open(os.path.join("locations","mars","underworld",f"e{epID}",f"e{epID}m{mapID}.json"), "w") as mapFile:
+                    json.dump(mapData,mapFile,indent=2)
--- scripts/init.lua
+++ scripts/init.lua
@@ -4,14 +4,17 @@ IS_UNLABELLED = variant:find("maps-u")
 -- Items
 print("Loading Items")
 Tracker:AddItems("items/items.json")
+print("")
 
 -- Logic
 print("Loading Logic")
 ScriptHost:LoadScript("scripts/logic/logic.lua")
+print("")
 
 -- Maps
 print("Loading Maps")
 Tracker:AddMaps("maps/maps.json")
+print("")
 
 -- Layout
 print("Loading Layouts")
@@ -22,17 +25,32 @@ Tracker:AddLayouts("layouts/settings.json")
 Tracker:AddLayouts("layouts/tabs.json")
 Tracker:AddLayouts("layouts/tracker.json")
 Tracker:AddLayouts("layouts/broadcast.json")
+print("")
 
 -- Locations
 print("Loading Locations")
-Tracker:AddLocations("locations/mars/mars.json")            -- Mars
-Tracker:AddLocations("locations/mars/overworld/e1.json")    -- KDitD
-Tracker:AddLocations("locations/mars/underworld/e1/e1m1.json")
-Tracker:AddLocations("locations/mars/underworld/e1/e1m2.json")
-Tracker:AddLocations("locations/mars/overworld/e2.json")    -- TSoH
-Tracker:AddLocations("locations/mars/overworld/e3.json")    -- I
-Tracker:AddLocations("locations/mars/overworld/e4.json")    -- TFC
-Tracker:AddLocations("locations/locations.json")            -- Mars (Original)
+Tracker:AddLocations("locations/mars/mars.json") -- Mars
+for e = 1,4
+do
+    Tracker:AddLocations(
+        "locations/mars/overworld/" ..
+        "e" .. e ..
+        ".json"
+    )
+    for m = 1,9
+    do
+        Tracker:AddLocations(
+            "locations/mars/underworld/" ..
+            "e" .. e ..
+            "/" ..
+            "e" .. e ..
+            "m" .. m ..
+            ".json"
+        )
+    end
+end
+-- Tracker:AddLocations("locations/locations.json")            -- Mars (Original)
+print("")
 
 -- AutoTracking for Poptracker
 if PopVersion and PopVersion >= "0.18.0" then
